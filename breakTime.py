b46164d938955f851d30f1328509f6dcef41677f
laurentmor <laurent.morissette@gmail.com>
2016-04-13 21:22:41 -0400

    create P1-Branch

--- breakTime.py
+++
@@ -1,8 +0,0 @@
-import time
-import webbrowser
-n=0
-print ("Program started on: "+time.ctime)
-while (n<3):
-    time.sleep(10)
-    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    n=n+1
